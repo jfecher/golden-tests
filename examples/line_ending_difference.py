b32429618cf48ca12757441d310d7fcb060d1271
Jake Fecher <jfecher11@gmail.com>
2020-08-02 17:33:07 -0400

    Fix bug where empty lines where sometimes not displayed

---
+++ examples/line_ending_difference.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+
+print("Test1\r\nTest2\n\n\n\nhi\n")
+
+# Change the number of empty lines here to test error output
+# expected stdout:
+# Test1
+# Test2
+# 
+# 
+# 
+# hi
--- src/diff_printer.rs
+++ src/diff_printer.rs
@@ -4,23 +4,35 @@ use std::fmt::{ Formatter, Display, Error };
 
 pub struct DiffPrinter(pub Changeset);
 
-fn fmt_lines<F, D: Display>(lines: &str, mut current_line: Option<&mut usize>,
-    f: &mut Formatter, colorizer: F) -> Result<(), Error>
-
+fn print_line_number<F, D: Display>(current_line: Option<usize>,
+        f: &mut Formatter, colorizer: &F) -> Result<Option<usize>, Error>
     where F: Fn(&str) -> D,
-          D: Display
 {
-    for line in lines.lines() {
-        if let Some(line) = &current_line {
-            write!(f, "{:3}| ", line)?;
-        } else {
-            write!(f, "{:3}| ", " ")?;
-        }
+    let line_number = current_line.as_ref().map_or(" ".to_string(), |line| line.to_string());
+    let line_number_string = format!("{:>3}| ", line_number);
+    write!(f, "{}", colorizer(&line_number_string))?;
+    Ok(current_line.map(|x| x + 1))
+}
 
-        writeln!(f, "{}", colorizer(line))?;
-        current_line.as_deref_mut().map(|x| *x += 1);
+fn fmt_lines<F, D: Display>(lines: &str, mut current_line: Option<usize>,
+        f: &mut Formatter, colorizer: F) -> Result<usize, Error>
+    where F: Fn(&str) -> D,
+{
+    current_line = print_line_number(current_line, f, &colorizer)?;
+
+    for character in lines.chars() {
+        if character == '\r' {
+            // Do nothing
+        } else if character == '\n' {
+            writeln!(f, "")?;
+            current_line = print_line_number(current_line, f, &colorizer)?;
+        } else {
+            write!(f, "{}", colorizer(&character.to_string()))?;
+        }
     }
-    Ok(())
+
+    writeln!(f, "")?;
+    Ok(current_line.unwrap_or(0))
 }
 
 impl Display for DiffPrinter {
@@ -29,20 +41,20 @@ impl Display for DiffPrinter {
         for i in 0 .. self.0.diffs.len() {
             match &self.0.diffs[i] {
                 Difference::Same(lines) => {
-                    fmt_lines(lines, Some(&mut line), f, |x| x.normal())?;
+                    line = fmt_lines(lines, Some(line), f, |x| x.normal())?;
                 },
                 Difference::Add(lines) => {
                     // Don't show/increment the line number if the previous change was a Removal
                     if i > 0 && matches!(self.0.diffs[i - 1], Difference::Rem(..)) {
                         fmt_lines(lines, None, f, |x| x.green())?;
                     } else {
-                        fmt_lines(lines, Some(&mut line), f, |x| x.green())?;
+                        line = fmt_lines(lines, Some(line), f, |x| x.green())?;
                     }
                 },
                 Difference::Rem(lines) => {
                     // Don't show/increment the line number unless the next change is an Addition
                     if i < self.0.diffs.len() - 1 && matches!(self.0.diffs[i + 1], Difference::Add(..)) {
-                        fmt_lines(lines, Some(&mut line), f, |x| x.red())?;
+                        line = fmt_lines(lines, Some(line), f, |x| x.red())?;
                     } else {
                         fmt_lines(lines, None, f, |x| x.red())?;
                     }
