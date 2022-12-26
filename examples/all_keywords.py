e93ab81c68f1b94dd5f389a700a0ba9eb3a1e758
Edvard Thörnros <edvard.thornros@gmail.com>
2022-12-27 00:36:49 +0100

    Fix the test output to be stable

--- examples/all_keywords.py
+++ examples/all_keywords.py
@@ -6,7 +6,7 @@ sys.exit(3)
 
 # args: -c 'print("test"); exec(open("examples/all_keywords.py").read())'
 # expected exit status: 3
-# expected stdout:test
+# expected stdout: test
 
-# expected stderr:error!
+# expected stderr: error!
 
