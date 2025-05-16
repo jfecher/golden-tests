9be5d9b4820ee354fe49d87500a37d111c10725e
Jake Fecher <jfecher11@gmail.com>
2025-05-16 11:19:37 -0500

    Add CLI arguments for 'base_args' and 'base_args_after'

--- src/main.rs
+++ src/main.rs
@@ -61,22 +61,30 @@ struct Args {
         help = "Update the expected output of each test file to match the actual output"
     )]
     overwrite: bool,
+
+    #[clap(long, help = "Arguments to add before the file name when running every test file")]
+    base_args: String,
+
+    #[clap(long, help = "Arguments to add after the file name when running every test file")]
+    base_args_after: String,
 }
 
 fn main() {
     let args = Args::parse();
 
-    let config = TestConfig::with_custom_keywords(
-        args.binary_path,
-        args.test_path,
-        &args.test_prefix,
-        &args.args_prefix,
-        &args.args_after_prefix,
-        &args.stdout_prefix,
-        &args.stderr_prefix,
-        &args.exit_status_prefix,
-        args.overwrite,
-    );
+    let config = TestConfig {
+        binary_path: args.binary_path,
+        test_path: args.test_path,
+        test_line_prefix: args.test_prefix,
+        test_args_prefix: args.args_prefix,
+        test_args_after_prefix: args.args_after_prefix,
+        test_stdout_prefix: args.stdout_prefix,
+        test_stderr_prefix: args.stderr_prefix,
+        test_exit_status_prefix: args.exit_status_prefix,
+        overwrite_tests: args.overwrite,
+        base_args: args.base_args,
+        base_args_after: args.base_args_after,
+    };
 
     config.run_tests().unwrap_or_else(|_| std::process::exit(1));
 }
